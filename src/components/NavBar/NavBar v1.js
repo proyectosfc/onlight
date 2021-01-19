0b57e110f25b66c78c054ce2006c9937cfb90707
Freddy Carrasco <freddycarrascosegura@gmail.com>
2021-01-19 00:33:54 -0300

    navbar

---
+++ src/components/NavBar/NavBar v1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+
+export const NavBar = () => {
+    
+   return (
+	<div className='containerNav'>
+	 <div className='logo'>
+		LOGO
+	 </div>
+	 <p>INICIO</p>
+	 <p>CATEGORIAS</p>
+	 <p>CARRITO</p>
+	</div>
+   )
+}
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+
+export const NavBar = () => {
+    
+   return (
+	<div className='containerNav'>
+	 <div className='logo'>
+		ONLIGHT - ILUMINACION
+	 </div>
+	 <p><a href="#">INICIO</a></p>
+	 <p><a href="#">CATEGORIAS</a></p>
+	 <p><a href="#">CARRITO</a></p>
+	</div>
+   )
+}
